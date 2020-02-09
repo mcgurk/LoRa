b173fab77353fc1601da34f31d541f6c37c135ae
Jarno Lehtinen <lehtinen@sci.fi>
2020-02-09 13:19:25 +0200

    Update receiver_mini.ino

--- RadioLib/receiver_mini.ino
+++ RadioLib/receiver_mini.ino
@@ -1,86 +1,49 @@
 #include <RadioLib.h>
 
-SX1276 lora = new Module(D8, D2, D3, D1); //NSS, DIO0, RST, DIO1
+//SX1276 lora = new Module(D8, D2, D3, D1); // NSS, DIO0, RST, DIO1
+SX1276 lora = new Module(D8, D2, D3); // NSS, DIO0, RST, DIO1
 
-void ICACHE_RAM_ATTR setFlag(void);
+void ICACHE_RAM_ATTR setFlag(void); // Arduinollä jätä tämä rivi pois
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
-  Serial.print(F("[SX1276] Initializing ... "));
-  // carrier frequency:           434.0 MHz
-  // bandwidth:                   125.0 kHz
-  // spreading factor:            9
-  // coding rate:                 7
-  // sync word:                   0x12
-  // output power:                17 dBm
-  // current limit:               100 mA
-  // preamble length:             8 symbols
-  // amplifier gain:              0 (automatic gain control)
-  //int state = lora.begin();
-  int state = lora.begin(868, 125, 10, 8, 0x77, 2);
+  Serial.print(F("Alustetaan SX1276..."));
+  int state = lora.begin(868, 125, 9, 7, 0x64); // Freq[MHz], BW[kHz], SF, CR, syncword
   if (state == ERR_NONE) {
-    Serial.println(F("success!"));
+    Serial.println(F("Alustus onnistui!"));
   } else {
-    Serial.print(F("failed, code "));
+    Serial.print(F("Alustus epäonnistui, koodi "));
     Serial.println(state);
     while (true);
   }
-
-  // set the function that will be called
-  // when new packet is received
   lora.setDio0Action(setFlag);
-
-  // start listening for LoRa packets
-  Serial.print(F("[SX1276] Starting to listen ... "));
+  Serial.print(F("Aloita LoRa-pakettien kuuntelu."));
   state = lora.startReceive();
   if (state == ERR_NONE) {
-    Serial.println(F("success!"));
+    Serial.println(F("Kuuntelutilan asetus onnistui!"));
   } else {
-    Serial.print(F("failed, code "));
+    Serial.print(F("Kuuntelutilan asetus epäonnistui, koodi "));
     Serial.println(state);
     while (true);
   }
-
-  // if needed, 'listen' mode can be disabled by calling
-  // any of the following methods:
-  //
-  // lora.standby()
-  // lora.sleep()
-  // lora.transmit();
-  // lora.receive();
-  // lora.readData();
-  // lora.scanChannel();
 }
 
-// flag to indicate that a packet was received
 volatile bool receivedFlag = false;
-
-// disable interrupt when it's not needed
 volatile bool enableInterrupt = true;
 
-// this function is called when a complete packet
-// is received by the module
-// IMPORTANT: this function MUST be 'void' type
-//            and MUST NOT have any arguments!
 void setFlag(void) {
-  // check if the interrupt is enabled
   if(!enableInterrupt) {
     return;
   }
-
-  // we got a packet, set the flag
   receivedFlag = true;
 }
 
 void loop() {
   // check if the flag is set
   if(receivedFlag) {
-    // disable the interrupt service routine while
-    // processing the data
+    // jätetään reagoimatta mahdollisiin jatkokeskeytyksiin
     enableInterrupt = false;
-
-    // reset flag
+    // nollataan lippu
     receivedFlag = false;
 
     // you can read received data as an Arduino String
@@ -94,46 +57,37 @@ void loop() {
     */
 
     if (state == ERR_NONE) {
-      // packet was successfully received
-      Serial.println(F("[SX1278] Received packet!"));
-
-      // print data of the packet
-      Serial.print(F("[SX1276] Data:\t\t"));
+      Serial.println(F("Vastaanotettiin paketti!"));
+      Serial.print(F("Data:\t\t"));
       Serial.println(str);
 
       // print RSSI (Received Signal Strength Indicator)
-      Serial.print(F("[SX1276] RSSI:\t\t"));
+      Serial.print(F("RSSI:\t\t"));
       Serial.print(lora.getRSSI());
       Serial.println(F(" dBm"));
 
       // print SNR (Signal-to-Noise Ratio)
-      Serial.print(F("[SX1276] SNR:\t\t"));
+      Serial.print(F("SNR:\t\t"));
       Serial.print(lora.getSNR());
       Serial.println(F(" dB"));
 
       // print frequency error
-      Serial.print(F("[SX1276] Frequency error:\t"));
+      Serial.print(F("Frequency error:\t"));
       Serial.print(lora.getFrequencyError());
       Serial.println(F(" Hz"));
 
     } else if (state == ERR_CRC_MISMATCH) {
-      // packet was received, but is malformed
-      Serial.println(F("[SX1276] CRC error!"));
-
-    } else {
-      // some other error occurred
-      Serial.print(F("[SX1276] Failed, code "));
+      Serial.println(F("CRC-tarkistussumma virhe!"));
+    } else { // jokin muu virhe kuin CRC-virhe
+      Serial.print(F("Paketin vastaanotto epäonnistui, koodi "));
       Serial.println(state);
-
     }
 
-    // put module back to listen mode
+    // asetetaan SX1276 takaisin kuuntelutilaan
     lora.startReceive();
 
-    // we're ready to receive more packets,
-    // enable interrupt service routine
+    // aktivoidaan keskeytysten käsittely uudelleen
     enableInterrupt = true;
   }
-  
-  delay(500);
+
 }
