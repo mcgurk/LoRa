71f776b90cf0afe8c3521328183cf70895f2a851
Jarno Lehtinen <lehtinen@sci.fi>
2020-11-18 21:33:08 +0200

    Update rf_sender.py

--- rf_sender.py
+++ rf_sender.py
@@ -25,15 +25,15 @@ print(spi.max_speed_hz)
 def send(data):
   # Goal: 1/0,001141*8 -> 7011,394Hz
   # Raspi4 600MHz/250MHz (powersave) multiplayer for real freq ~3,38???
-  #spi.max_speed_hz = 25000 # ~7392Hz? works
-  #spi.max_speed_hz = 32000 # ~9488Hz? works
-  #spi.max_speed_hz = 16000 # ~4720Hz? doesn't work
-  #spi.max_speed_hz = 20000 # ~5936Hz? doesn't work
-  #spi.max_speed_hz = 40000 # ~11792Hz? doesn't work
-  #spi.max_speed_hz = 23000 # ~6800Hz? works
-  #spi.max_speed_hz = 24000 # ~7152Hz? works
-  #spi.max_speed_hz = 23500 # ~6960Hz? works
-  spi.max_speed_hz = 23700 # ~7008Hz? works
+  #spi.max_speed_hz = 25000 # ~7392Hz works
+  #spi.max_speed_hz = 32000 # ~9488Hz works
+  #spi.max_speed_hz = 16000 # ~4720Hz doesn't work
+  #spi.max_speed_hz = 20000 # ~5936Hz doesn't work
+  #spi.max_speed_hz = 40000 # ~11792Hz doesn't work
+  #spi.max_speed_hz = 23000 # ~6800Hz works
+  #spi.max_speed_hz = 24000 # ~7152Hz works
+  #spi.max_speed_hz = 23500 # ~6960Hz works
+  spi.max_speed_hz = 23700 # ~7008Hz works
   print(spi.max_speed_hz)
   for i in range(6):
     time.sleep(0.01)
