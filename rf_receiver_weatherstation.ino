df5063e33226f1e36b443edcba1b48dfcb6a7c26
Jarno Lehtinen <lehtinen@sci.fi>
2021-02-11 12:49:43 +0200

    Update rf_receiver_weatherstation.ino

--- rf_receiver_weatherstation.ino
+++ rf_receiver_weatherstation.ino
@@ -19,13 +19,11 @@ ICACHE_RAM_ATTR void ISR() {
     receiving = 0;
     return;
   }
-  //if ( delta < 11000 && delta > 9000) flag = 1;
   if ( (delta > 10000) && (delta < 10040) ) {
     p = 0;
     receiving = 1;
     return;
   }
-  //if (p > 27) receiving = 0;
   if (receiving) {
     buf[p++] = delta;
     if (p == 28) {
@@ -42,16 +40,27 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(GPIO), ISR, RISING);
 }
 
+//char message[29];
+uint32_t message;
+
 void loop() {
   if (flag) {
-    //Serial.println("HEP!");
     for (int i = 0; i < 28; i++) {
+      //uint8_t b = (buf[i] > 4000) ? 1 : 0;
+      //bitWrite(message, 27-i, b);
+      bitWrite(message, 27-i, ((buf[i] > 4000) ? 1 : 0) );
+      //bitWrite(message, 27-i, 1);
+    }
+    Serial.println(message, BIN);
+    flag = 0;
+    //Serial.println("HEP!");
+    /*for (int i = 0; i < 28; i++) {
       Serial.print(" ");
       Serial.print(buf[i]);
     }
     Serial.println();
     //Serial.print("Millis: "); Serial.print(millis()); Serial.print(" delta: "); Serial.println(delta);
-    flag = 0;
+    flag = 0;*/
   }
   /*uint32_t temp = counter;
   Serial.println(temp-old_counter);
