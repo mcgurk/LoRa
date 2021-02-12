d67503519af1401849ea4926ea912b5657fd8458
Jarno Lehtinen <lehtinen@sci.fi>
2021-02-13 00:49:29 +0200

    Update rf_receiver_weatherstation_ARDUINO-HW.ino

--- RF/rf_receiver_weatherstation_ARDUINO-HW.ino
+++ RF/rf_receiver_weatherstation_ARDUINO-HW.ino
@@ -14,29 +14,67 @@ void setup() {
   TCCR1B = _BV(ICNC1) | _BV(ICES1) | _BV(CS11); //CS11: prescaler 8, ICES1: rising edge, ICNC1: Input Capture Noise Canceller
 }
 
+uint8_t isValid(uint32_t msg) {
+  uint32_t sum = 0;
+  for (int i = 0; i < 6; i++) {
+    sum += msg & 0xf;
+    msg >>= 4;
+  }
+  sum--;
+  return ((msg&0xf) == (sum&0xf));
+}
 
 void loop() {
-  if (buf[0][28]) {
-    buf[0][28] = 0;
-    Serial.print(" value: "); Serial.println(buf[0][0]);
-    //Serial.print(" value: "); Serial.println(temp);
+  for (int slot = 0; slot < 16; slot++)
+  if (buf[slot][28]) {
+    buf[slot][28] = 0;
+    uint32_t message = 0;
+    for (int i = 0; i < 28; i++) {
+      bitWrite(message, 27-i, ((buf[slot][i] > 8000) ? 1 : 0) );
+    }
+    if (!isValid(message)) {
+      Serial.println("Checksum error!");
+      return;
+    }
+    int16_t t = ((uint16_t)message) >> 4;
+    float temperature = ((float)t)/10;
+    Serial.print("Slot:"); Serial.print(slot); Serial.print(" millis(): "); Serial.print(millis()); Serial.print(" value: "); Serial.println(temperature);
   }
 }
 
 
 ISR(TIMER1_CAPT_vect) { // ICIE1
+  static uint32_t delta;
+  static uint8_t p;
+  static uint8_t receiving = 0; 
   static uint16_t oldCounter;
+  static uint8_t slot = 0;
   uint16_t newCounter = ICR1; //ICR1: Input Capture Register
-  uint16_t delta = newCounter - oldCounter;
+  delta = newCounter - oldCounter; // 1 unit in delta is 0,5us (1s/16000000Hz*8prescaler)
   oldCounter = newCounter;
-  if (delta > 20000 && delta < 20050) {
-    buf[0][0] = delta;
-    buf[0][28] = 0xff;
+
+  if (delta < 4800) {
+    receiving = 0;
+    return;
   }
-  //temp = ICR1;
-  //temp = TCNT1;
-  //uint16_t counter = ICR1; //ICR1: Input Capture Register
-  //counter = ICR1; //ICR1: Input Capture Register
-  //TIFR1 = 0xff; // Clear all pending TIMER1 interrupt flags
+  if (delta > 20200) {
+    receiving = 0;
+    return;
+  }
+  if ( (delta > 20000) && (delta < 20100) ) {
+    p = 0;
+    receiving = 1;
+    return;
+  }
+  if (receiving) {
+    buf[slot][p++] = delta;
+    if (p == 28) {
+      buf[slot][28] = 0xff;
+      slot++;
+      slot &= 0xf;
+      receiving = 0;
+    }
+  }
+
 }
 
